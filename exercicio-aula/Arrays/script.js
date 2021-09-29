bb20f616eec2112106e960784e6c6eb4f2d9e1a8
valentinahastenreiter <valentina.hastenreiter@gmail.com>
2021-09-29 20:22:56 -0300

    Aula de sábado JS

---
+++ exercicio-aula/Arrays/script.js
@@ -0,0 +1,31 @@
+// 1. crie uma array com objetos, cada objeto com pelo menos 4 propriedades
+
+let objetosDecasa = ['vaso', 'mousepad', 'panela']
+
+let vasoDecorativo = {
+    nome: 'Vaso Decorativo',
+    tipoDeUso: usado,
+    cor: ['azul', 'preto'],
+    característica: ['espelhado', 'resistente'],
+}
+
+let mousepad = {
+    nome: mousepad,
+    tamanho: novo,
+    cor: ['preto', 'amarelo', 'vermelho'],
+    característica: ['macio', 'resistente', 'fácil de limpar'],
+}
+
+
+let panela = {
+    nome: panela,
+    tamanho: novo,
+    cor: ['preto', 'vermelho', 'branco'],
+    característica: ['antiaderente', 'resistente', 'alumínio'],
+}
+
+
+
+// 2. use métodos para acessar valores das propriedades desses objetos, e encontrar objetos 
+//pelo indice da lista.
+
