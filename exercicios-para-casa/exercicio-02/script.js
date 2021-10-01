d82d11865684f50cd232e32dd856cc879cf38c4d
AidaFig <aidafigueiredo8@gmail.com>
2021-10-01 20:42:14 -0300

    exercicio 02 respondido

--- exercicios-para-casa/exercicio-02/script.js
+++ exercicios-para-casa/exercicio-02/script.js
@@ -1,4 +1,13 @@
 // considere a lista com objetos:
+
+/*
+1. ETAPA1: Mostre no console as todas as propriedades e valores da variável pessoas;
+
+2. ETAPA2: Crie uma nova array que retorne o nome e a profissão das pessoas que vivem no Brasil, para as que não vivem no Brasil retorne o nome da pessoa com a mensagem: " infelizmente não mora na área pesquisada"; 
+
+3. ETAPA3: Crie uma nova array que retorne o país onde moram as pessoas que trabalham como desenvolvedoras frontend;
+*/
+
 const pessoas = [
     {
         nome: 'Joana',
@@ -21,10 +30,26 @@ const pessoas = [
         pais: 'Brasil',
     },
 ];
-/*
-1. ETAPA1: Mostre no console as todas as propriedades e valores da variável pessoas;
 
-2. ETAPA2: Crie uma nova array que retorne o nome e a profissão das pessoas que vivem no Brasil, para as que não vivem no Brasil retorne o nome da pessoa com a mensagem: " infelizmente não mora na área pesquisada"; 
 
-3. ETAPA3: Crie uma nova array que retorne o país onde moram as pessoas que trabalham como desenvolvedoras frontend;
-*/
+console.log(pessoas)
+
+const infos = pessoas.map((pessoa) => {
+    if(pessoa.pais == 'Brasil'){
+    return `${pessoa.nome}, ${pessoa.profissao}`;
+    }else {
+        return `${pessoa.nome} infelizmente não mora na área pesquisada`
+    }
+    
+})
+
+console.log(infos)
+
+const pais = pessoas.map((pessoa) => {
+    if(pessoa.profissao == 'desenvolvedora frontend'){
+        return `${pessoa.nome} vive em ${pessoa.pais}`
+    } 
+})
+
+console.log(pais)
+
