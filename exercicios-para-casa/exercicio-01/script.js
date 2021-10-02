3e02136c663e39b0bf828d52a54bdff367d21d89
Celina de Souza Marinho <celina.marinho@gmail.com>
2021-10-01 23:34:17 -0300

    exercicios resolvidos

--- exercicios-para-casa/exercicio-01/script.js
+++ exercicios-para-casa/exercicio-01/script.js
@@ -8,3 +8,22 @@
 4. ETAPA4: Veja se existe o item com valor "salada" dentro da sua array; 
 
 */
+1.
+const familia = ["Esmeraldina", "Valdir", "Rita", "Rafael", "Celina", "Rogério"];
+
+2.
+const elemento1Familia = familia.shift();
+console.log(elemento1Familia);
+console.log(familia);
+
+3.
+const familiaSort = familia.sort();
+console.log (familia);
+
+4.
+familia.includes("salada");
+console.log(familia.includes("salada"));
+
+
+
+
--- exercicios-para-casa/exercicio-02/script.js
+++ exercicios-para-casa/exercicio-02/script.js
@@ -22,9 +22,43 @@ const pessoas = [
     },
 ];
 /*
-1. ETAPA1: Mostre no console as todas as propriedades e valores da variável pessoas;
+1. ETAPA1: Mostre no console as todas as propriedades e valores da variável pessoas;*/
+pessoas.forEach(function(pessoa){
+    console.log(pessoa.nome);
+    console.log(pessoa.profissao);
+    console.log(pessoa.pais);
+})
 
-2. ETAPA2: Crie uma nova array que retorne o nome e a profissão das pessoas que vivem no Brasil, para as que não vivem no Brasil retorne o nome da pessoa com a mensagem: " infelizmente não mora na área pesquisada"; 
 
-3. ETAPA3: Crie uma nova array que retorne o país onde moram as pessoas que trabalham como desenvolvedoras frontend;
-*/
+2. //ETAPA2: Crie uma nova array que retorne o nome e a profissão das pessoas que vivem no Brasil, para as que não vivem no Brasil retorne o nome da pessoa com a mensagem: " infelizmente não mora na área pesquisada"; 
+
+pessoas.map(function(pessoa){
+    return {nome: pessoa.nome, profissao: pessoa.profissao}
+}
+)
+
+const verificacao = pessoas.map(function(pessoa){
+    if (pessoa.pais == "Brasil"){
+        return {nome: pessoa.nome, profissao: pessoa.profissao}
+
+    } else {
+
+        return {nome: pessoa.nome, mensagem: "infelizmente não mora na área pesquisada"}
+    }
+
+})
+
+console.log(verificacao);
+
+//3. ETAPA3: Crie uma nova array que retorne o país onde moram as pessoas que trabalham como desenvolvedoras frontend;
+
+const verificacao2 = pessoas.map(function(pessoa){
+    if (pessoa.profissao == "desenvolvedora frontend"){
+        return pessoa.pais
+    
+    }
+}
+)
+
+console.log(verificacao2);
+
