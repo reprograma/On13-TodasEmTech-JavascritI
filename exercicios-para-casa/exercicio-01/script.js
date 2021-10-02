412161c5236eab0d54709515c6f627e25bb5a4e2
NayaraNicacio <nayaranicacio42@gmail.com>
2021-10-01 22:30:11 -0300

    Atividade-pra-casa1

--- exercicios-para-casa/exercicio-01/script.js
+++ exercicios-para-casa/exercicio-01/script.js
@@ -1,10 +1,20 @@
-/*
-1. ETAPA1: Crie uma variável que contenha uma array com até 6 itens;
 
-2. ETAPA2: Remova o primeiro item desta array;
+//1. ETAPA1: Crie uma variável que contenha uma array com até 6 itens;
 
-3. ETAPA3: Ordene a sua lista em ordem alfabética ou numérica (de forma crescente e/ou descrescente);
+let salada = String ['rucula', 'tomate', 'pepino', 'cebola', 'azeitona', 'pimentao'];
 
-4. ETAPA4: Veja se existe o item com valor "salada" dentro da sua array; 
+console. log (salada);
 
-*/
+//2. ETAPA2: Remova o primeiro item desta array;
+salada.shift()
+console. log(salada);
+
+//3. ETAPA3: Ordene a sua lista em ordem alfabética ou numérica (de forma crescente e/ou descrescente);
+
+salada.sort();
+console. log(salada);
+
+//4. ETAPA4: Veja se existe o item com valor "salada" dentro da sua array; 
+
+console. log(salada.includes("salada"));
+    
