d2878874a50fb363f0719656cedbb4919f04461f
Aline Viana <alinemaciel018@gmail.com>
2021-10-04 10:49:38 -0300

    exerc

--- exercicios-para-casa/exercicio-02/script.js
+++ exercicios-para-casa/exercicio-02/script.js
@@ -24,7 +24,8 @@ const pessoas = [
 /*
 1. ETAPA1: Mostre no console as todas as propriedades e valores da variável pessoas;
 
-2. ETAPA2: Crie uma nova array que retorne o nome e a profissão das pessoas que vivem no Brasil, para as que não vivem no Brasil retorne o nome da pessoa com a mensagem: " infelizmente não mora na área pesquisada"; 
+2. ETAPA2: Crie uma nova array que retorne o nome e a profissão das pessoas que vivem no Brasil, 
+para as que não vivem no Brasil retorne o nome da pessoa com a mensagem: " infelizmente não mora na área pesquisada"; 
 
 3. ETAPA3: Crie uma nova array que retorne o país onde moram as pessoas que trabalham como desenvolvedoras frontend;
 */
@@ -35,8 +36,8 @@ console.log(pessoas[2].nome)
 console.log(pessoas[3].nome)
 console.log(pessoas[2].profissao)
 console.log(pessoas[3].profissao)
+console.log(pessoas[1].pais) 
+console.log(pessoas[3].pais.includes('Brasil')) 
 
-console.log(pessoas[2].pais.includes(Brasil)) 
-console.log(pessoas[4].pais.includes(Brasil)) 
 
 
