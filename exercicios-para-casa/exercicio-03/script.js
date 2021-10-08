5dce8444fee8cae4e4e655c63276b4df97d8bd8f
marina-alves <mariinaalvesr@gmail.com>
2021-10-08 10:37:39 -0300

    exercicios-js1-marina

--- exercicios-para-casa/exercicio-03/script.js
+++ exercicios-para-casa/exercicio-03/script.js
@@ -9,3 +9,43 @@
 4. ETAPA4: Diga qual é a média de idade des funcionáries da empresa
 
 */
+
+let funcionaries = [
+    {
+        nome: 'Marina',
+        cargo: 'Gerente',
+        idade: 29,
+        salario: 10000,
+        anosEmpresa: 1,
+    },
+    {
+        nome: 'Wesley',
+        cargo: 'atendente',
+        idade: 24,
+        salario: 8000,
+        anosEmpresa: 3,
+    },
+    {
+        nome: 'Lucas',
+        cargo: 'Pizzaiolo',
+        idade: 28,
+        salario: 11000,
+        anoEmpresa: 2,
+    },
+    {
+        nome: 'Kelwen',
+        cargo: 'Auxiliar-pizzaiolo',
+        idade: 26,
+        salario: 8000,
+        anosEmpresa: 1,
+    },
+    {
+        nome: 'Edson',
+        cargo: 'Auxiliar',
+        idade: 32,
+        salario: 6000,
+        anosEmpresa: 2,
+    }
+];
+
+console.log(funcionaries[1].nome)
