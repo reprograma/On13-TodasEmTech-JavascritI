4de6165c2fa0123445839cc2f2fc4abed7d1dd2e
Marcela Ismael <marcela.ismsilva@outlook.com>
2021-12-02 23:49:43 -0300

    exercicio-casa

--- exercicios-para-casa/exercicio-02/script.js
+++ exercicios-para-casa/exercicio-02/script.js
@@ -1,30 +1,91 @@
-// considere a lista com objetos:
-const pessoas = [
+// considerar a lista com objetos:
+const  pessoas  =  [
     {
-        nome: 'Joana',
-        profissao: 'atriz',
-        pais: 'Canadá',
-    },
+        nome : 'Joana' ,
+        profissao : 'atriz' ,
+        pais : 'Canadá' ,
+    } ,
     {
-        nome: 'Emma',
-        profissao: 'desenvolvedora frontend',
-        pais: 'EUA',
-    },
+        nome : 'Emma' ,
+        profissao : 'desenvolvedora frontend' ,
+        pais : 'EUA' ,
+    } ,
     {
-        nome: 'Julia',
-        profissao: 'economista',
-        pais: 'Brasil',
-    },
+        nome : 'Julia' ,
+        profissao : 'economista' ,
+        pais : 'Brasil' ,
+    } ,
     {
-        nome: 'Claudia',
-        profissao: 'desenvolvedora frontend',
-        pais: 'Brasil',
-    },
-];
-/*
-1. ETAPA1: Mostre no console as todas as propriedades e valores da variável pessoas;
+        nome : 'Claudia' ,
+        profissao : 'desenvolvedora frontend' ,
+        pais : 'Brasil' ,
+    } ,
+] ;
+/ *
+1. ETAPA1: Mostre no console como todas as propriedades e valores da variável pessoas;
+2. ETAPA2: Crie uma nova array que retorne o nome e a profissão das pessoas que vivem no Brasil, para as que não vivem no Brasil retorne o nome da pessoa com uma mensagem: "infelizmente não mora na área pesquisada"; 
+3. ETAPA3: Crie uma nova array que retorna ao país onde moram como pessoas que trabalham como desenvolvedoras frontend;
+* /
 
-2. ETAPA2: Crie uma nova array que retorne o nome e a profissão das pessoas que vivem no Brasil, para as que não vivem no Brasil retorne o nome da pessoa com a mensagem: " infelizmente não mora na área pesquisada"; 
+// etapa 1
+console . log ( pessoas ) ;
 
-3. ETAPA3: Crie uma nova array que retorne o país onde moram as pessoas que trabalham como desenvolvedoras frontend;
-*/
+// etapa 2
+    // considerar a lista com objetos:
+    const  pessoas  =  [ {
+            nome : 'Joana' ,
+            profissao : 'atriz' ,
+            pais : 'Canadá' ,
+        } ,
+        {
+            nome : 'Emma' ,
+            profissao : 'desenvolvedora frontend' ,
+            pais : 'EUA' ,
+        } ,
+        {
+            nome : 'Julia' ,
+            profissao : 'economista' ,
+            pais : 'Brasil' ,
+        } ,
+        {
+            nome : 'Claudia' ,
+            profissao : 'desenvolvedora frontend' ,
+            pais : 'Brasil' ,
+        } ,
+    ] ;
+    / *
+    1. ETAPA1: Mostre no console como todas as propriedades e valores da variável pessoas;
+    2. ETAPA2: Crie uma nova array que retorne o nome e a profissão das pessoas que vivem no Brasil, para as que não vivem no Brasil retorne o nome da pessoa com uma mensagem: "infelizmente não mora na área pesquisada"; 
+    3. ETAPA3: Crie uma nova array que retorna ao país onde moram como pessoas que trabalham como desenvolvedoras frontend;
+    * /
+
+    // ETAPA 1
+    console . log ( pessoas ) ;
+
+    // ETAPA 2
+
+    const  filtroPaisBrasil  =  pessoas.filtro ( função  (Brasil) {
+        voltar  Brasil . pais  ==  'Brasil'
+    } ) ;
+
+    const  filtroEstrangeiros  =  pessoas . filtro ( função  ( Estrangeiros )  {
+        voltar  Estrangeiros . pais  ! =  'Brasil'
+    } ) ;
+
+    const  resultadoFinalBrasil  =  filtroPaisBrasil . map ( função ( Brasil ) {  retornar  Brasil . nome  +  '-'  +  Brasil . profissao } ) ;
+
+    console . log ( resultadoFinalBrasil )
+
+    const  resultadoFinalEstrangeiros  =  filtroEstrangeiros . map ( função ( Estrangeiros ) {  voltar  Estrangeiros . nome  +  '-'  +  'Infelizmente não mora na área pesquisada' } ) ;
+
+    console . log ( resultadoFinalEstrangeiros )
+
+
+    // ETAPA 3
+     const  devsFrontEnd  =  pessoas . filtro ( função  (devs)  {
+        return  devs.profissao  == 'frontend desenvolvedora'
+    } ) ;
+
+    const  resultadoFinalDevsFrontEnd  =  devsFrontEnd . map ( função ( devs ) {  return  devs . nome  +  '-'  +  devs . pais  +  '-'  +  devs . profissao } ) ;
+
+    console . log ( resultadoFinalDevsFrontEnd )
--- exercicios-para-casa/exercicio-03/script.js
+++ exercicios-para-casa/exercicio-03/script.js
@@ -1,11 +1,87 @@
-/* 
+/ * 
+1. ETAPA1: Crie uma lista que receba as informações: nome, carga, idade, salário e anos de empresa pelo menos 5 funções de alguma empresa;
+2. ETAPA2: consulta um aumento de 10% ao salário de funções que leva 3 anos de empresa ou mais, aos que não possuem 3 anos de empresa dê um aumento de 5%;
+3. ETAPA3: Crie uma nova lista apenas com funções que têm mais do que 40 anos; 
+4. ETAPA4: Diga qual é a média de idade das funções da empresa
+* /
 
-1. ETAPA1: Crie uma lista que receba as informações: nome, cargo, idade, salário e anos de empresa de pelo menos 5 funcionáries de alguma empresa;
+// etapa 1
+// ETAPA 1
+const  funcionarios1  =  [
+    {
+        nome : 'Eduarda' ,
+@@ -47,4 +47,73 @@ const funcionarios1 = [
+        salário : 1045 ,
+        anosDeEmpresa : 1 ,
+    } ,
+] 
+]
 
-2. ETAPA2: Adicione um aumento de 10% ao salário de funcionáries que tiverem 3 anos de empresa ou mais, aos que não possuem 3 anos de empresa dê um aumento de 5%;
 
-3. ETAPA3: Crie uma nova lista apenas com funcionáries que tenham mais do que 40 anos; 
+// const devsFrontEnd = pessoas.filter (function (devs) {
+// // return devs.profissao == 'desenvolvedora frontend'
+// //});
 
-4. ETAPA4: Diga qual é a média de idade des funcionáries da empresa
+// // const resultadoFinalDevsFrontEnd = devsFrontEnd.map (function (devs) {return devs.nome + '-' + devs.pais + '-' + devs.profissao});
 
-*/
+// // console.log (resultadoFinalDevsFrontEnd)
+
+
+// ETAPA2: consulta um aumento de 10% ao salário de funções que leva 3 anos de empresa ou mais, aos que não possuem 3 anos de empresa dê um aumento de 5%;
+// funcionários com 3 anos ou mais de empresa
+const  funcionarios3Anos  =  funcionarios1 . filtro ( função  ( antigos )  {
+    voltar  antigos . anosDeEmpresa  > =  3
+} ) ;
+
+const  aumento  =  funcionarios3Anos . map ( função ( antigos ) { 
+    voltar  antigos . nome  +  '-'  +  antigos . salário  +  '-'  +  'Salário reajustado:'  +  ( antigos . salário  *  0.1  +  salário . antigos )
+} ) ;
+
+console . log ( aumento )
+
+// funcionários com menos de 3 anos de empresa
+const  funcionariosMenosDe3Anos  =  funcionarios1 . filtro ( função  ( novos )  {
+    return  novos . anosDeEmpresa  <  3
+} ) ;
+
+const  aumento1  =  funcionariosMenosDe3Anos . map ( função ( novos ) { 
+    return  novos . nome  +  '-'  +  novos . salário  +  '-'  +  'Salário reajustado:'  +  ( novos . salário  *  0,05  +  novos . salário )
+} ) ;
+
+console . log ( aumento1 )
+
+
+// 3. ETAPA3: Crie uma nova lista apenas com funções que têm mais do que 40 anos; 
+const  funcionarios2  =  [
+    {
+        nome : 'Sandra' ,
+        cargor : 'Coordenadora' ,
+        idade : 46 ,
+        salário : 9000 ,
+        anosDeEmpresa : 6 ,
+    } ,
+    {
+        nome : 'Geovane' ,
+        cargor : 'UX Desing' ,
+        idade : 45 ,
+        salário : 7000 ,
+        anosDeEmpresa : 3 ,
+    } ,
+    {
+        nome : 'Gustavo' ,
+        cargor : 'Desenvolvedor frontend Senior' ,
+        idade : 42 ,
+        salário : 12.000 ,
+        anosDeEmpresa : 1 ,
+    } ,
+    {
+        nome : 'Maria' ,
+        cargor : 'Consultora Senior' ,
+        idade : 48 ,
+        salário : 2.800 ,
+        anosDeEmpresa : 5 ,
+    } ,
+]
+
+// 4. ETAPA4: Diga qual é a média de idade des funcionáries da empresa
+const  mediaFuncionarios 
